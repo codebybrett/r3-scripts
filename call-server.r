69bc3f4fbcf0c77975e0e383a42ad291e53cb129
Brett Handley <codebybrett@the-handleys.net>
2013-06-23 16:06:53 +1000

    Added some basic collision/interception detection.

--- call-server.r
+++ call-server.r
@@ -21,7 +21,7 @@ REBOL [
 		limitations under the License.
 	}
 	History: [
-		1.3.0 [22-Jun-2013 "Changed behaviour of /receive /get-response." "Brett Handley"]
+		1.3.0 [23-Jun-2013 "Added shared secrets to reduce risk of collision/interception. Changed behaviours. Bug fixes." "Brett Handley"]
 		1.2.0 [22-Jun-2013 "Renamed (was interactive-cmd-server.r3.r), fix /nosysinput." "Brett Handley"]
 		1.1.0 [21-Jun-2013 "Significant changes. Now useable." "Brett Handley"]
 		1.0.0 [13-Jun-2013 "Initial unfinished REBOL 3 Alpha version." "Brett Handley"]
@@ -52,39 +52,31 @@ REBOL [
 ;
 ;		Example: Controlling PSFTP.exe:
 ;
-;			tell-psftp: func [
-;				{Sends command to utility, returns response.}
-;				cmd [char! string! block!] {Command to send - must be a single command no newline.}
-;				/expect prompt [string! block!] {Prompt to expect following a response from the command. Default is "psftp> ".}
-;				/local response
-;			] [
-;				if not expect [prompt: {psftp> }]
-;				if block? cmd [cmd: rejoin cmd]
-;				server/send join cmd newline
-;				response: server/get-response prompt
-;				if 'closed = response/status [
-;					do make error! rejoin [{The command has exited with message: } mold trim copy response/string]
-;				]
-;				response/string
-;			]
-;		
 ;			server: make-call-server {"C:\Program Files (x86)\PuTTY\psftp.exe"}
 ;			server/startup
-;			server/get-response {psftp> }
-;			print tell-psftp {help}
+;			print mold server/get-response {psftp> }
+;			server/send {help^/}
+;			print mold server/get-response {psftp> }
 ;			server/send {quit^/}
 ;			server/shutdown
 ;
-;		I hope that Call in REBOL 3 will be upgraded enough to make this script redundant.
+;		The listener port is tied up for the duration of /startup. After that it can be reused
+;		by another instance. So to use multiple simulataneous command calls, start them one
+;		after the other with /startup, or you could you could set them up on different
+;		listener ports by setting /listen.
 ;
 ;
 ;	Requirements:
 ;
-;		* Windows.
-;		* This Rebol 3 program currently requires Rebol 2 to run.
+;		* This Rebol 3 program currently requires Rebol 2 for the sender and receiver processes.
+;
+;		* As written, needs Windows, but see call-and-pipe below, perhaps
+;		  a simple change will get it working on other platforms.
 ;
 ;
-;	Comments/Warnings:
+;	Comments:
+;
+;		It's my hope that Call in REBOL 3 will be upgraded enough to make this script redundant.
 ;
 ;		I couldn't find a simpler approach so this is what I came up with:
 ;
@@ -92,17 +84,18 @@ REBOL [
 ;			object sends commands to the first REBOL process which just	emits them to its
 ;			output, which is then piped to the command, which in turns has it's output piped
 ;			to the second REBOL process, which finally sends the output back to the Command
-;			server object.
+;			server object. In addition there is a cmd.exe process that ties them together.
 ;
-;		It may or may not work for you - I found it works for PING and PSFTP (Putty) but not properly
-;		for FTP (part of windows).
+;		It may or may not work for you. On windows, I found it works for PING and PSFTP (Putty)
+;		but not properly for FTP.
 ;
-;		No checking is done on the connections, so you could have problems (security?) if some other
-;		network program tries to connect to these processes, or you run more than one instance of this
-;		without changing the network port.
+;		To avoid the risk of collisions/interception shared secrets are passed and checked
+;		between the processes. This script checks the secret sent by the sender and the receiver
+;		and the sender checks the secret this script sends it. I wanted have the receiver
+;		check a secret this script sends it, but I had a problem I could not fix, probably not important.
 ;
-;		I have included many comments as much for myself as anyone else as this is my first Rebol 3
-;		networking script.
+;		I have included many comments as much for myself as anyone else as this is my first
+;		significant Rebol 3 script.
 ;
 ;
 ;	Rebol 3 Alpha - problems.
@@ -114,14 +107,10 @@ REBOL [
 ;		console programs and you need to rewrite the helper networking code.
 ;
 ;
-;	To Do (maybe):
+;	To Do:
 ;
-;		o Replace use of Does, Has and Func with Funct.
+;		o Take advantage of Rebol 3 code structuring including using Funct instead of Does, Has, Func.
 ;
-;		o Add handshaking to prevent collisions and add connection security.
-;			- To allow port range for listeners - a component to manage this resource.
-;			- To check that helper is legit.
-;			- To allow multiple simultaneous command calls.
 ;
 ; ---------------------------------------------------------------------------------------------------------------------
 ;
@@ -174,12 +163,13 @@ REBOL [
 ;			The return value is like that of tokenise-response. When STATUS is CLOSED,
 ;			an additional key/value pair (CLOSED-BY) describes why the connection was closed.
 ;
-;				- A STATUS of the word CLOSED indicates the connection was closed. This can
-;				  considered an error because the response was not properly completed.
+;				- A STATUS of the word CLOSED indicates the connection was closed.
+;				  This should be treated as an error when your prompt is not none,
+;				  because the response is not properly completed.
 ;				  If syserr is being redirected, which is the default, the response
-;				  string may contain the error message from the command. The connection may be
+;				  string may contain an error message from the command. The connection may be
 ;				  closed by the called program (CLOSED-BY CMD-EXIT), or by get-response
-;				  when a timeout occurs (CLOSED-BY TIMEOUT).
+;				  when a timeout occurs (CLOSED-BY TIMEOUT) while waiting for prompt.
 ;
 ;
 ;
@@ -191,7 +181,7 @@ REBOL [
 ;
 ;			When the function finds a delimiter it returns a block of key/value pairs:
 ;
-;				string [string!]
+;				data [string! binary!]
 ;
 ;					- This is the buffered response up to the prompt or close of connection.
 ;
@@ -217,7 +207,7 @@ call-output: func [
 	]
 	server/shutdown
 	if error? result [do result]
-	result/string
+	result/data
 ]
 
 
@@ -232,8 +222,8 @@ make-call-server: func [
 
 	context [
 
-		;;;log: func [x][write/append %cmd-test.log.txt rejoin [newline now {: } reform :x]]
 		log: none
+		;;; log: func [x][write/append %cmd-test.log.txt rejoin [newline now {: } reform :x]]
 
 		; Port to listen on.
 		listen: 8000 ; This needs to be configurable.
@@ -248,6 +238,12 @@ make-call-server: func [
 		; Response-timeout time. Used by get-response.
 		; Amount of time to wait for data to be emitted by the called program.
 		response-timeout: 1
+
+		; Remove CR?
+		remove-CR: ('windows = system/platform/1)
+
+		; As string!
+		string-data: true
 
 		; Need input helper?
 		need-input: not nosysinput
@@ -277,16 +273,28 @@ make-call-server: func [
 		trace-sending: trace-send
 		trace-receiving: trace-receive
 
+		; This does the call and setups up piping between sender, program and receiver.
+		; I have broken it out here in case someone can get it to work on other platforms.
+		call-and-pipe: func [
+			/local cmdstr
+		][
+			cmdstr: either need-errors [rejoin [command-string { 2>&1}]] [command-string]
+			call rejoin [{cmd /c } sender-cmd cmdstr receiver-cmd]
+		]
+
 		; Startup
 		startup: func [
-			/local listener cmdstr
+			/local listener
+			chk-l chk-s chk-r
 		] [
 
 			;
 			; Initialise.
 
-			receive-buffer: copy {}
-			response-buffer: copy {}
+			receive-buffer: copy #{}
+			response-buffer: copy either string-data [{}][#{}]
+
+			foreach var [chk-l chk-s chk-r] [set :var form random/secure 9999999]
 
 			if not integer? startup-timeout [do make error! {startup-timeout must be an integer!}]
 			if not integer? response-timeout [do make error! {response-timeout must be an integer!}]
@@ -307,9 +315,12 @@ make-call-server: func [
 						sender: port
 						sender/awake: func [event] [
 							log ["sender: " :event/type]
-							if 'close = event/type [
-								closeports
-								return true
+							switch event/type [
+								read [return true]
+								close [
+									closeports
+									return true
+								]
 							]
 							false
 						]
@@ -324,7 +335,7 @@ make-call-server: func [
 						switch event/type [
 							read [
 								if trace-receiving [log ["received: " mold to string! receiver/data]]
-								append receive-buffer to string! receiver/data
+								append receive-buffer receiver/data
 								clear receiver/data ; Remove processed data from port buffer.
 								return true ; Return from Wait.
 							]
@@ -348,6 +359,8 @@ make-call-server: func [
 			sender-cmd: either need-input [
 				rejoin [
 					rebol.exe { -w --do "wait svr: open/direct/no-wait tcp://localhost:} listen
+					{ if not equal? copy svr form } chk-l { [quit]}
+					{ insert svr } chk-s
 					{ x: copy svr while [not none? x][prin x wait svr x: copy svr] close svr"}
 					{ | }
 				]
@@ -360,16 +373,17 @@ make-call-server: func [
 			receiver-cmd: rejoin [
 				{ | }
 				rebol.exe { -w --do "s: open/direct tcp://localhost:} listen
-				{ set-modes system/ports/input [lines: false] b: make string! n: } max-receiver-packet
-				{ forever [r: read-io system/ports/input clear b n if r < 0 [break] insert s replace/all b CR {}] close svr"}
+				{ set-modes system/ports/input [lines: false]}
+				{ insert s } chk-r
+				{ b: make string! n: } max-receiver-packet
+				{ forever [r: read-io system/ports/input clear b n if r < 0 [break] insert s b] close svr"}
 			]
 
 			;
 			; Call command piping input from sender and piping its output to receiver.
-		
-			cmdstr: either need-errors [rejoin [command-string { 2>&1}]] [command-string]
-			call rejoin [{cmd /c } sender-cmd cmdstr receiver-cmd]
 
+			call-and-pipe
+		
 			;
 			; Wait for connection from sender REBOL process. Timeout indicates failure of the command.
 
@@ -378,7 +392,7 @@ make-call-server: func [
 				if none? wait [listener startup-timeout] [
 					close listener
 					closeports
-					do make error! rejoin [{The following command appears to have failed: } cmdstr]
+					do make error! rejoin [{Could not establish connection to sender, command may have failed: } command-string]
 				]
 			]
 
@@ -389,7 +403,7 @@ make-call-server: func [
 			if none? wait [listener startup-timeout] [
 				close listener
 				closeports
-				do make error! rejoin [{Could not setup pipeline to the command: } cmdstr]
+				do make error! rejoin [{Could not establish connection with receiver for the command: } command-string]
 			]
 
 			;
@@ -398,6 +412,31 @@ make-call-server: func [
 			close listener
 			log "listener closed."
 
+			if need-input [
+				log "send sender secret"
+				if none? :sender [do make error! rejoin [{Sender terminated connection unexpectedly: } command-string]]
+				write sender chk-l wait 0.01 ; Send secret.
+
+				log "check sender secret"
+				if none? :sender [do make error! rejoin [{Sender terminated connection unexpectedly: } command-string]]
+				read sender wait [sender 0.1] ; Wait for secret or close.
+				if not connection? [
+					do make error! rejoin [{Sender did not accept secret for command: } command-string]
+				]
+				if not parse sender/data [remove chk-s to end] [
+					closeports
+					do make error! rejoin [{Unknown process tried connect as sender for command: } command-string]
+				]
+			]
+
+			log "check receiver secret"
+			read receiver wait [receiver 0.1] ; Wait for secret from receiver.
+			if not parse receive-buffer [remove chk-r to end] [
+				closeports
+				do make error! rejoin [{Unknown process tried to connect as receiver for command: } command-string]
+			]
+
+			log "startup completed."
 			exit
 
 		]
@@ -406,7 +445,8 @@ make-call-server: func [
 			{Send the command some input.}
 			data [string! char!]
 		] [
-			if none? :sender [do make error! rejoin [{Sender connection to } mold cmdstr { is closed.}]]
+			log "Send."
+			if none? :sender [do make error! rejoin [{Sender connection to } mold command-string { is closed.}]]
 			if trace-sending [log ["sending: " mold :data]]
 			write sender to binary! data
 			wait 0.01 ; Need a wait for the write to occur.
@@ -428,15 +468,23 @@ make-call-server: func [
 			{Get's next response from receiver. Returns none if timeout or connection closed.}
 			/local response
 		] [
-			if none? :receiver [do make error! rejoin [{Receiver connection to } mold cmdstr { is closed.}]]
+			log "Receive."
+			if none? :receiver [do make error! rejoin [{Receiver connection to } mold command-string { is closed.}]]
 			log "read receiver"
 			read receiver
 			wait-response
 			while [not empty? receive-buffer][
-				append any [response response: copy {}] receive-buffer
+				append any [response response: copy #{}] receive-buffer
 				clear receive-buffer
-				read receiver
-				wait [receiver 0.1] ; Wait for a short time to see if more packets are coming.
+				if found? :receiver [ ; Connection still open.
+					log "read receiver"
+					read receiver
+					wait [receiver 0.1] ; Wait for a short time to see if more packets are coming.
+				]
+			]
+			if found? response [
+				if remove-cr [replace/all response CR {}]
+				if string-data [response: to string! response]
 			]
 			response
 		]
@@ -462,7 +510,7 @@ make-call-server: func [
 		tokenise-response: func [delimiters /local a b result] [
 			foreach string delimiters [
 				if parse/all response-buffer [to string a: string b: to end] [
-					result: compose [string (copy/part response-buffer a) status (string)]
+					result: compose [data (copy/part response-buffer a) status (string)]
 					remove/part response-buffer b
 					return result
 				]
@@ -505,7 +553,7 @@ make-call-server: func [
 					connection? ; Loss of connection indicates command exited.
 					lesser? now/precise timeout-time ; A timeout, indicates a problem with client logic.
 				][continue]
-				result: compose [string (response-buffer) status closed closed-by (either connection? ['timeout]['cmd-exit])]
+				result: compose [data (response-buffer) status closed closed-by (either connection? ['timeout]['cmd-exit])]
 				response-buffer: none ; Nothing left and connection closed.
 				shutdown
 				break
